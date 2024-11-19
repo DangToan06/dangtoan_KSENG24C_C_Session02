bc00d681b22662c6286ee737461a8f5cdbaf6a47
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-19 14:42:35 +0700

    Add files via upload

---
+++ Ex02.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a = 4, b = 5;
+	int c = a + b;
+	
+	printf("tong la %d", c);
+
+	return 0;
+}
+
---
+++ Ex04.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int canhHinhVuong = 2;
+	int chuVi = 4*canhHinhVuong;
+	int dienTich = pow(canhHinhVuong,2);
+	
+	printf("chi vi hinh chu nhat là %d\ndien tich hinh chu nhat la %d", chuVi, dienTich);
+
+	return 0;
+}
+
