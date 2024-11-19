d27f8f0921386459b952bd0de28a6db35dc9cdef
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-19 15:49:23 +0700

    Add files via upload

---
+++ Ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+//	kieu du lieu so nguyen
+	int a;
+//	kieu du lieu so thu
+	float b;
+//	kieu du lieu ky tu
+	char c;
+
+	return 0;
+}
+
---
+++ Ex05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int chieuDai = 4, chieuRong = 2;
+	int chuVi = 2*chieuDai + 2*chieuRong;
+	int dienTich = chieuDai*chieuRong;
+	
+	printf("chi vi hinh chu nhat là %d\ndien tich hinh chu nhat la %d", chuVi, dienTich);
+
+	return 0;
+}
+
