59683fdb4f2771d3149e196b9583d63b61dd1e1a
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-19 21:11:30 +0700

    Update Ex01.c

--- Ex01.c
+++ Ex01.c
@@ -3,10 +3,12 @@
 int main(){
 //	kieu du lieu so nguyen
 	int a;
-//	kieu du lieu so thu
+//	kieu du lieu so thuc 
 	float b;
 //	kieu du lieu ky tu
 	char c;
+//	kieu du lieu so thu do chinh xac cao hơn
+	double d;
 
 	return 0;
 }
